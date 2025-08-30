cd808f2ca9f4f5a47e61dc872e6b9bb7d31f5cca
johnpaulnwazuoke <johnpaulnwazuoke@github.com>
2025-08-30 14:34:58 +0100

    error from undeclared variable

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
 output "primary_endpoint"  { value = aws_elasticache_replication_group.this.primary_endpoint_address }
 output "reader_endpoint"   { value = aws_elasticache_replication_group.this.reader_endpoint_address }
 output "port"              { value = aws_elasticache_replication_group.this.port }
-output "security_group_id" { value = aws_security_group.redis.id }
 output "subnet_group_name" { value = aws_elasticache_subnet_group.this.name }
